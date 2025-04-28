a280a16dc0e0b9388885a61bd1f3d5d367d3ea75
Siderea <hutornoyaa@gmail.com>
2025-04-28 09:30:09 +0300

    add error handling

--- src/main/java/homerep/homerepoauth/services/orderservice/PaymentService.java
+++ src/main/java/homerep/homerepoauth/services/orderservice/PaymentService.java
@@ -3,10 +3,12 @@ package homerep.homerepoauth.services.orderservice;
 import homerep.homerepoauth.config.HomeRepProperties;
 import homerep.homerepoauth.models.orderservice.PaymentType;
 import homerep.homerepoauth.models.orderservice.dto.DefaultResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
+@Slf4j
 @Service
 public class PaymentService {
     private final WebClient webClient;
@@ -21,12 +23,24 @@ public class PaymentService {
         return webClient.patch()
                 .uri(ORDER_SERVICE_URL+ "/activate?paymentName={name}", paymentName)
                 .retrieve()
+
+                .onStatus(HttpStatusCode::isError, clientResponse ->
+                        clientResponse.bodyToMono(String.class)
+                                .flatMap(errorBody -> Mono.error(new RuntimeException("Error from Order Service: " + errorBody)))
+                )
                 .bodyToMono(Object.class);
     }
     public Mono<Object> deactivatePayment(String paymentName) {
         return webClient.patch()
-                .uri(ORDER_SERVICE_URL+ "deactivate?paymentName={name}", paymentName)
+                .uri(ORDER_SERVICE_URL+ "/deactivate?paymentName={name}", paymentName)
+
                 .retrieve()
+
+                .onStatus(HttpStatusCode::isError, clientResponse ->
+                        clientResponse.bodyToMono(String.class)
+                                .flatMap(errorBody -> Mono.error(new RuntimeException("Error from Order Service: " + errorBody)))
+                )
+
                 .bodyToMono(Object.class);
     }
 }
